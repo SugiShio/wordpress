2fe1cb8d0c989643c3c59b4e7e5c6bf25cee16dc
Shoko S <fruitmachine0@gmail.com>
2018-08-19 10:49:41 +0900

    初期状態

---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const WebpackNotifierPlugin = require('webpack-notifier');
+const path = require('path');
+
+const config = {
+  mode: 'development',
+  useSourceMap: true,
+}
+
+module.exports = {
+  mode: config.mode,
+  context: path.join(__dirname, 'wp-content/themes'),
+  entry: {'origin': './origin/assets/app.js'},
+  output: {
+    path: path.join(__dirname, 'wp-content/themes'),
+    filename: '[name]/public/js/index.js'
+  },
+  module: {
+    rules: [
+    {
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        use: [
+        {
+          loader: 'css-loader',
+          options: {
+            sourceMap: config.useSourceMap,
+            importLoaders: 2
+          },
+        },
+        {
+          loader: 'postcss-loader',
+          options: {
+            sourceMap: config.useSourceMap,
+            plugins: [
+            require('autoprefixer')({grid: true})
+            ]
+          },
+        },
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: config.useSourceMap,
+          }
+        }
+        ]
+      }),
+    },
+    {
+      test: /\.(png|jpg|gif|svg)$/,
+      use: [
+      {
+        loader: 'url-loader',
+        options: {
+          name: '[path][name].[ext]',
+          outputPath: '',
+          publicPath: '../',
+          limit: 1048576,
+        }
+      }
+      ]
+    },
+    {
+      test: /\.js$/,
+      use: [
+      {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+          ['env', {'modules': false}]
+          ]
+        }
+      }
+      ]
+    },
+    {
+      test: /\.vue$/,
+      use: [
+      {
+        loader: 'vue-loader',
+      }
+      ]
+    }
+    ]
+  },
+  plugins: [
+  new ExtractTextPlugin('[name]/public/css/style.css'),
+  new VueLoaderPlugin(),
+  new WebpackNotifierPlugin(),
+  ],
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.common.js'
+    }
+  },
+};
+
--- wp-content/themes/origin/functions.php
+++ wp-content/themes/origin/functions.php
@@ -1,1 +1,7 @@
 <?php
+    function my_scripts() {
+        wp_enqueue_style('my-style', get_template_directory_uri() . '/public/css/style.css');
+        wp_enqueue_script('jQuery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js');
+        wp_enqueue_script('my-scripts', get_template_directory_uri() . '/public/js/index.js');
+    }
+    add_action('wp_enqueue_scripts', 'my_scripts');

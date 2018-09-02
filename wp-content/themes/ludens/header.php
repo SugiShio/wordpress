9494297df721d7fd4bb95ed7f80033a8885740df
Shoko S <fruitmachine0@gmail.com>
2018-09-02 22:13:37 +0900

    トップコーディング

--- wp-content/themes/ludens/header.php
+++ wp-content/themes/ludens/header.php
@@ -2,7 +2,9 @@
 <html <?php language_attributes(); ?>>
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <?php /*
   <meta name="viewport" content="width=device-width">
+  */ ?>
   <!--[if lt IE 9]>
   <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
 <![endif]-->
--- wp-content/themes/ludens/index.php
+++ wp-content/themes/ludens/index.php
@@ -1,3 +1,161 @@
 <?php get_header(); ?>
-hello
+
+<div class="wrapper">
+  <header class="header">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.svg" alt="LUDENS ISESAKI WEDDING VILLA" class="header__logo">
+    <div class="contact-box">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tell-number.svg" alt="" class="contact-box__tell-number">
+      <ul class="contact-box__links">
+        <li class="contact-box__item">
+          <a href="#" class="contact-box__link">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/reserve.svg" alt="">
+          </a>
+        </li>
+        <li class="contact-box__item">
+          <a href="#" class="contact-box__link">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/document.svg" alt="">
+          </a>
+        </li>
+        <li class="contact-box__item">
+          <a href="#" class="contact-box__link">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact.svg" alt="">
+          </a>
+        </li>
+      </ul>
+    </div>
+    <ul class="sns-links">
+      <li class="sns-links__item">
+        <a href="#" class="sns-links__link">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/facebook.png" alt="">
+        </a>
+      </li>
+      <li class="sns-links__item">
+        <a href="#" class="sns-links__link">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/instagram.png" alt="">
+        </a>
+      </a>
+    </ul>
+  </header>
+
+  <nav class="menu">
+    <ul class="menu-list">
+      <li class="menu-list__item">
+        <a href="#" class="menu-list__link">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/top.svg" alt="トップ" class="menu-list__image">
+        </a>
+      </li>
+      <li class="menu-list__item">
+        <a href="#" class="menu-list__link">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/concept.svg" alt="コンセプト" class="menu-list__image">
+        </a>
+      </li>
+      <li class="menu-list__item">
+        <a href="#" class="menu-list__link">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/facility.svg" alt="施設" class="menu-list__image">
+        </a>
+      </li>
+      <li class="menu-list__item">
+        <a href="#" class="menu-list__link">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/fair.svg" alt="フェア" class="menu-list__image">
+        </a>
+      </li>
+      <li class="menu-list__item">
+        <a href="#" class="menu-list__link">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/plan.svg" alt="プラン" class="menu-list__image">
+        </a>
+      </li>
+      <li class="menu-list__item">
+        <a href="#" class="menu-list__link">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cuisine.svg" alt="料理" class="menu-list__image">
+        </a>
+      </li>
+    </ul>
+  </nav>
+
+  <main>
+    <div class="main-view">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/mainview.jpg" alt="Espoir LumineuxからLUDENS WEDDING VILLAへ...大人のラグジュアリー空間へ生まれ変わります。">
+    </div>
+
+    <div class="top-content">
+      <div class="top-content__top-box">
+        <section class="pickup">
+          <ul class="pickup__list">
+            <li class="pickupItem">
+              <div class="pickupItem__image">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/fair.jpg" alt="">
+              </div>
+              <div class="pickupItem__body">
+                <h2 class="pickupItem__title">【サンプルフェア】テキストテキストテキスト♪</h2>
+                <p class="pickupItem__text">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+              </div>
+            </li>
+            <li class="pickupItem">
+              <div class="pickupItem__image">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/fair.jpg" alt="">
+              </div>
+              <div class="pickupItem__body">
+                <h2 class="pickupItem__title">【サンプルフェア】テキストテキストテキスト♪</h2>
+                <p class="pickupItem__text">テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+              </div>
+            </li>
+          </ul>
+          <a href="#" class="pickup__link">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/show-all-fair.svg" alt="全てのフェアを見る">
+          </a>
+        </section>
+        <div class="top-content__top-links">
+          <a href="#" class="top-content__top-banner">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/staff-blog.jpg" alt="スタッフブログ">
+          </a>
+          <a href="#" class="top-content__top-banner">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/event-calender.jpg" alt="イベントカレンダー">
+          </a>
+        </div>
+      </div>
+
+      <ul class="top-content__links">
+        <li class="top-content__item">
+          <a href="#" class="top-content__link">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/concept.jpg" alt="コンセプト">
+          </a>
+        </li>
+        <li class="top-content__item">
+          <a href="#" class="top-content__link">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/plan.jpg" alt="プラン">
+          </a>
+        </li>
+        <li class="top-content__item">
+          <a href="#" class="top-content__link">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/fair.jpg" alt="フェア">
+          </a>
+        </li>
+        <li class="top-content__item">
+          <a href="#" class="top-content__link">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/facility.jpg" alt="施設">
+          </a>
+        </li>
+        <li class="top-content__item">
+          <a href="#" class="top-content__link">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/gallery.jpg" alt="ギャラリー">
+          </a>
+        </li>
+        <li class="top-content__item">
+          <a href="#" class="top-content__link">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/access.jpg" alt="アクセス">
+          </a>
+        </li>
+      </ul>
+    </div>
+  </main>
+
+  <footer class="footer">
+    <div class="footer__copyright">
+      <small>
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/copyright.svg" alt="Copyright ©︎ 0000000 All Rights Reserved.">
+      </small>
+    </div>
+  </footer>
+
+</div>
 <?php get_footer(); ?>

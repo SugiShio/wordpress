eeea438bdae56a0b12329f9e334ba6d48e98aacf
Shoko S <fruitmachine0@gmail.com>
2018-09-03 20:02:20 +0900

    住所修正

--- wp-content/themes/ludens/page-access.php
+++ wp-content/themes/ludens/page-access.php
@@ -6,9 +6,9 @@
 
   <main class="access">
     <div class="map">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3214.3961527439064!2d139.1917681153623!3d36.32694758004881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601ee8ad9c1e8007%3A0xfea84f0e4cbbb1e!2z5LyK5Yui5bSO6aeF!5e0!3m2!1sja!2sjp!4v1535897590598" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d989.1883664808443!2d139.2147434997718!3d36.294374123620756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601ee7cfb4c0e639%3A0x17ee7aa2674bf52b!2z44CSMzcyLTAwMzMg576k6aas55yM5LyK5Yui5bSO5biC5Y2X5Y2D5pyo55S6IOOCqOOCueODneODr-ODvOODq-ODq-ODn-ODjQ!5e0!3m2!1sja!2sjp!4v1535971362801&zoom=1" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
       <p class="map__info">
-        ルーデンス伊勢崎<br>住所:群馬県〇〇市0-0-0<br>アクセス:〇〇線〇〇駅より徒歩2分
+        ルーデンス伊勢崎<br>住所:群馬県伊勢崎市南千木町2555<br>tel:0270-27-5445<br>最寄駅:伊勢崎駅／伊勢崎駅南出口より車で15分<br>アクセス:【両毛線をご利用のお客様】 伊勢崎駅／JR両毛線伊勢崎駅南出口より車で15分 【お車でお越しのお客様】 北関東自動車道駒形ICより車で15分 【新幹線をご利用のお客様】 上越新幹線本庄早稲田駅より車で25分
       </p>
     </div>
 

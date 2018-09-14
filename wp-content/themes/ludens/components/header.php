4d9205af09e3929d78048b4f41d93e022278304f
Shoko S <fruitmachine0@gmail.com>
2018-09-14 23:55:48 +0900

    見学予約・資料請求ページ

--- wp-content/themes/ludens/components/header.php
+++ wp-content/themes/ludens/components/header.php
@@ -7,12 +7,12 @@
       <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tell-number.png" alt="" class="contact-box__tell-number">
       <ul class="contact-box__links">
         <li class="contact-box__item">
-          <a href="<?php echo get_permalink(get_page_by_path('coming-soon')->ID); ?>" class="contact-box__link">
+          <a href="<?php echo get_permalink(get_page_by_path('reserve-input')->ID); ?>" class="contact-box__link">
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/reserve.png" alt="">
           </a>
         </li>
         <li class="contact-box__item">
-          <a href="<?php echo get_permalink(get_page_by_path('coming-soon')->ID); ?>" class="contact-box__link">
+          <a href="<?php echo get_permalink(get_page_by_path('request-input')->ID); ?>" class="contact-box__link">
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/document.png" alt="">
           </a>
         </li>
--- wp-content/themes/ludens/page.php
+++ wp-content/themes/ludens/page.php
@@ -1,7 +1,22 @@
 <?php get_header(); ?>
 
+<section class="contact">
+<?php if(have_posts()): while(have_posts()): the_post();?>
+  <div class="content-head">
+    <div class="content-head__main">
+      <h2 class="content-head__title">
+        <?php the_title(); ?>
+      </h2>
+    </div>
+    <p class="content-head__description"><?php echo post_custom('subtitle'); ?></p>
+  </div>
 
-<section class="coming-soon">
-<img src="<?php echo get_template_directory_uri(); ?>/assets/images/comingsoon_l.jpg" alt="Coming Soon">
+  <div class="content-main">
+    <div class="contact__form">
+      <?php the_content(); ?>
+    </div>
+  </div>
+<?php endwhile; endif; ?>
 </section>
+
 <?php get_footer(); ?>

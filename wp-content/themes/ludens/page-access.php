1622a7f87895afed1eeef0318b9f2d857cc38ff6
Shoko S <fruitmachine0@gmail.com>
2018-10-08 09:32:50 +0900

    アクセスページスマホ対応

--- wp-content/themes/ludens/page-access.php
+++ wp-content/themes/ludens/page-access.php
@@ -1,10 +1,29 @@
 <?php get_header(); ?>
-  <section class="access">
-    <div class="map">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d989.1883664808443!2d139.2147434997718!3d36.294374123620756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601ee7cfb4c0e639%3A0x17ee7aa2674bf52b!2z44CSMzcyLTAwMzMg576k6aas55yM5LyK5Yui5bSO5biC5Y2X5Y2D5pyo55S6IOOCqOOCueODneODr-ODvOODq-ODq-ODn-ODjQ!5e0!3m2!1sja!2sjp!4v1535971362801&zoom=1" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
-      <p class="map__info">
-        ルーデンス伊勢崎 ウエディングヴィラ<br>住所:群馬県伊勢崎市南千木町2555<br>tel:0270-75-2777<br>最寄駅:伊勢崎駅／伊勢崎駅南出口より車で15分<br>アクセス:【両毛線をご利用のお客様】 伊勢崎駅／JR両毛線伊勢崎駅南出口より車で15分 【お車でお越しのお客様】 北関東自動車道駒形ICより車で15分 【新幹線をご利用のお客様】 上越新幹線本庄早稲田駅より車で25分
-      </p>
+<section class="access">
+  <div class="content-head">
+    <div class="content-head__main">
+      <h2 class="content-head__title">
+        ACCESS
+      </h2>
+      <span class="content-head__sub">アクセス</span>
     </div>
-  </section>
+  </div>
+
+  <article class="content-main">
+    <div class="map">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d989.1883664808443!2d139.2147434997718!3d36.294374123620756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601ee7cfb4c0e639%3A0x17ee7aa2674bf52b!2z44CSMzcyLTAwMzMg576k6aas55yM5LyK5Yui5bSO5biC5Y2X5Y2D5pyo55S6IOOCqOOCueODneODr-ODvOODq-ODq-ODn-ODjQ!5e0!3m2!1sja!2sjp!4v1535971362801&zoom=1" width="800" height="600" frameborder="0" style="border:0" allowfullscreen class="map__iframe"></iframe>
+      <h2 class="map__title">ルーデンス伊勢崎 ウエディングヴィラ</h2>
+      <dl class="map__info">
+        <dt class="map__info-title">住所</dt>
+        <dd class="map__info-content">群馬県伊勢崎市南千木町2555</dd>
+        <dt class="map__info-title">電話番号</dt>
+        <dd class="map__info-content">0270-75-2777</dd>
+        <dt class="map__info-title">最寄駅</dt>
+        <dd class="map__info-content">0270-75-2777</dd>
+        <dt class="map__info-title">アクセス</dt>
+        <dd class="map__info-content">【両毛線をご利用のお客様】<br>伊勢崎駅／JR両毛線伊勢崎駅南出口より車で15分<br>【お車でお越しのお客様】<br>北関東自動車道駒形ICより車で15分<br>【新幹線をご利用のお客様】<br>上越新幹線本庄早稲田駅より車で25分</dd>
+      </dl>
+    </div>
+  </article>
+</section>
 <?php get_footer(); ?>

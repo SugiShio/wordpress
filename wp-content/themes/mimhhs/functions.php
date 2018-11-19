a1bbaf5073ad0b33b82c24a11efbd648a05d347e
Shoko Sugito <fruitmachine0@gmail.com>
2018-11-19 23:59:13 +0900

    cssとjsのコンパイル移行、案外すんなりできた

--- wp-content/themes/mimhhs/functions.php
+++ wp-content/themes/mimhhs/functions.php
@@ -17,7 +17,7 @@
     function mimhhs_scripts() {
         wp_enqueue_style('mimhhs-style', get_template_directory_uri() . '/public/css/style.css');
         wp_enqueue_script('jQuery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js');
-        wp_enqueue_script('mimhhs-scripts', get_template_directory_uri() . '/public/js/script.js');
+        wp_enqueue_script('mimhhs-scripts', get_template_directory_uri() . '/public/js/index.js');
     }
     add_action('wp_enqueue_scripts', 'mimhhs_scripts');
 

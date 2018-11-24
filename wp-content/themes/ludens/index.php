a8f359d3dec0a37216dcb742ef60fb59cf886855
Shoko Sugito <fruitmachine0@gmail.com>
2018-11-24 20:52:00 +0900

    修正細かいのたち

--- wp-content/themes/ludens/index.php
+++ wp-content/themes/ludens/index.php
@@ -54,48 +54,48 @@
     <li class="top-content__item">
       <a href="<?php echo get_permalink(get_page_by_path('concept')->ID); ?>" class="top-content__link">
         <span class="top-content__link-text">
-          <span class="top-content__link-text-main">CONCEPT</span>
           <span class="top-content__link-text-sub">コンセプト</span>
+          <span class="top-content__link-text-main">CONCEPT</span>
         </span>
       </a>
     </li>
     <li class="top-content__item">
       <a href="<?php echo get_permalink(get_page_by_path('plan')->ID); ?>" class="top-content__link">
         <span class="top-content__link-text">
-          <span class="top-content__link-text-main">PLAN</span>
           <span class="top-content__link-text-sub">プラン</span>
+          <span class="top-content__link-text-main">PLAN</span>
         </span>
       </a>
     </li>
     <li class="top-content__item">
       <a href="https://zexy.net/wedding/c_7770020837/" class="top-content__link" target="_blank">
         <span class="top-content__link-text">
-          <span class="top-content__link-text-main">FAIR</span>
           <span class="top-content__link-text-sub">フェア</span>
+          <span class="top-content__link-text-main">FAIR</span>
         </span>
       </a>
     </li>
     <li class="top-content__item">
       <a href="<?php echo get_permalink(get_page_by_path('facility')->ID); ?>" class="top-content__link">
         <span class="top-content__link-text">
-          <span class="top-content__link-text-main">FACILITY</span>
           <span class="top-content__link-text-sub">施設</span>
+          <span class="top-content__link-text-main">FACILITY</span>
         </span>
       </a>
     </li>
     <li class="top-content__item">
       <a href="<?php echo get_permalink(get_page_by_path('coming-soon')->ID); ?>" class="top-content__link">
         <span class="top-content__link-text">
-          <span class="top-content__link-text-main">GALLERY</span>
           <span class="top-content__link-text-sub">ギャラリー</span>
+          <span class="top-content__link-text-main">GALLERY</span>
         </span>
       </a>
     </li>
     <li class="top-content__item">
       <a href="<?php echo get_permalink(get_page_by_path('access')->ID); ?>" class="top-content__link">
         <span class="top-content__link-text">
-          <span class="top-content__link-text-main">ACCESS</span>
           <span class="top-content__link-text-sub">アクセス</span>
+          <span class="top-content__link-text-main">ACCESS</span>
         </span>
       </a>
     </li>

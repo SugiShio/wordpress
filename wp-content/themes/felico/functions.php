b8fc9338f253c3d87b5e1c4b845d93d7d6c6da4d
Shoko Sugito <fruitmachine0@gmail.com>
2019-07-11 22:29:01 +0900

    投稿タイプ追加

--- wp-content/themes/felico/functions.php
+++ wp-content/themes/felico/functions.php
@@ -1,7 +1,7 @@
 <?php
 function felico_setup() {
     add_theme_support('title-tag');
-    add_theme_support('post-thumbnails');
+    add_theme_support('post-thumbnails', array( 'post','achievements' ));
 }
 add_action('after_setup_theme', 'felico_setup');
 
@@ -11,8 +11,28 @@ function felico_scripts() {
 }
 add_action('wp_enqueue_scripts', 'felico_scripts');
 
-function register_my_menu() {
+function felico_init() {
+  // メニュー
   register_nav_menu('default', 'デフォルト' );
   register_nav_menu('home', 'ホーム' );
+
+  $post_types = [
+    ['label'=>'実績', 'term'=>'achievements', 'has_archive'=>true, 'menu_position'=>5, 'menu_icon'=>'dashicons-star-filled'],
+    ['label'=>'スタッフの声', 'term'=>'staff', 'has_archive'=>false, 'menu_position'=>5, 'menu_icon'=>'dashicons-star-filled'],
+  ];
+  foreach($post_types as $post_type) {
+    register_post_type($post_type['term'],
+      array(
+        'labels' => array(
+          'name' => $post_type['label'],
+        ),
+        'public' => true,
+        'has_archive' => $post_type['has_archive'],
+        'menu_position' => $post_type['menu_position'],
+        'menu_icon' => $post_type['menu_icon'],
+        'supports' => array('title', 'editor', 'thumbnail', 'custom-fields',)
+      )
+    );
+  };
 }
-add_action( 'init', 'register_my_menu' );
+add_action( 'init', 'felico_init' );

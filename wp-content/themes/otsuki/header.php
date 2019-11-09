4bb205b1499bcff409570c6d461c334775d3a55e
Shoko Sugito <fruitmachine0@gmail.com>
2019-11-09 19:55:40 +0900

    ヘッダースタイル

--- wp-content/themes/otsuki/header.php
+++ wp-content/themes/otsuki/header.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width">
   <!--[if lt IE 9]>
-  <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
-<![endif]-->
-<?php wp_head(); ?>
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
+  <![endif]-->
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php include 'menu.php' ?>
+  <?php include 'menu.php' ?>
+  <div class="o-wrapper">

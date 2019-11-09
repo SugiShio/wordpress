78543a7560bef79a6a935ddc4694537a39063e10
Shoko Sugito <fruitmachine0@gmail.com>
2019-11-09 14:14:02 +0900

    pug使う・babelアップデート

--- webpack.config.js
+++ webpack.config.js
@@ -78,7 +78,7 @@ module.exports = (env = {}) => {
             {
               loader: 'babel-loader',
               options: {
-                presets: ['@babel/preset-env']
+                presets: [['@babel/preset-env']]
               }
             }
           ]

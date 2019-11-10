bb10eab3e2ca0095da21fdc288dc6fd729817efe
Shoko Sugito <fruitmachine0@gmail.com>
2019-11-10 21:48:22 +0900

    タイトルのスタイル

--- wp-content/themes/otsuki/archive-works.php
+++ wp-content/themes/otsuki/archive-works.php
@@ -1,18 +1,27 @@
 <?php get_header(); ?>
-<?php
+<main class="o-main">
+  <div class="o-title">
+    <h2>
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/titles/works.svg" alt="works">
+    </h2>
+    <span class="o-title__sub">作品</span>
+  </div>
+
+  <?php
   $posts = get_posts(array(
     'post_type' => 'works',
     'order' => 'ASC'
   ));
   ?>
-<ul>
-  <?php foreach($posts as $post) : ?>
-  <li>
-    <?php setup_postdata( $post ); ?>
-    <?php echo get_the_title(); ?>
-    <?php wp_reset_postdata(); ?>
-  </li>
-  <?php endforeach; ?>
-</ul>
+  <ul>
+    <?php foreach($posts as $post) : ?>
+    <li>
+      <?php setup_postdata( $post ); ?>
+      <?php echo get_the_title(); ?>
+      <?php wp_reset_postdata(); ?>
+    </li>
+    <?php endforeach; ?>
+  </ul>
+</main>
 
 <?php get_footer(); ?>
--- wp-content/themes/otsuki/page-biography.php
+++ wp-content/themes/otsuki/page-biography.php
@@ -1,3 +1,9 @@
 <?php get_header(); ?>
-biography
+<main class="o-main">
+  <div class="o-title">
+    <h2>
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/titles/biography.svg" alt="biography">
+    </h2>
+  </div>
+</main>
 <?php get_footer(); ?>

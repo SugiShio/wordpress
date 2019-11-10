dd9eb442456ec83ddc881f5b50e5469619cb1281
Shoko Sugito <fruitmachine0@gmail.com>
2019-11-10 18:27:31 +0900

    トップページ大方

--- wp-content/themes/otsuki/functions.php
+++ wp-content/themes/otsuki/functions.php
@@ -1,4 +1,27 @@
 <?php
+$sns = array('youtube', 'twitter','instagram','facebook');
+$url_base = array(
+  'youtube' => "https://www.youtube.com/user/ACCOUNT_ID",
+  'twitter' => "https://twitter.com/ACCOUNT_ID",
+  'instagram' => "https://www.instagram.com/ACCOUNT_ID",
+  'facebook' => "https://www.facebook.com/ACCOUNT_ID",
+
+);
+$post = get_post(get_page_by_path('config'));
+setup_postdata( $post );
+$featured_video = get_field('video');
+
+foreach($sns as $item) {
+  if(get_field($item)) {
+    $accounts[] = array(
+      "name" => $item,
+      "id" => get_field($item),
+      "url" => str_replace("ACCOUNT_ID", get_field($item), $url_base[$item])
+    );
+  }
+};
+wp_reset_postdata();
+
 function o_scripts() {
   wp_enqueue_style('o-style', get_template_directory_uri() . '/public/css/style.css');
   wp_enqueue_script('o-scripts', get_template_directory_uri() . '/public/js/index.js');
--- wp-content/themes/otsuki/index.php
+++ wp-content/themes/otsuki/index.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
-<section>
+<section class="o-mainview">
   <p>Freelance Guitarist/Producer, based in Tokyo</p>
 </section>
 
 <?php /* Schedule */ ?>
-<section>
+<section class="o-show">
   <?php
   $posts = get_posts(array(
     'post_type' => 'schedule',
@@ -13,11 +13,29 @@
   ));
   $post = $posts[0];
   setup_postdata( $post );
+  $date = get_field('date');
+  $place = get_field('place');
   ?>
+  <h2 class="o-show__title">Upcoming show</h2>
 
-  <?php echo post_custom('date'); ?>
-  <?php echo get_the_title(); ?>
+  <div class="o-show__item">
+    <time class="o-show__date" datetime="<?php echo $date; ?>">
+      <?php echo date("Y.m.d D", strtotime($date));?>
+    </time>
+    <div class="o-show__itemtitle">
+      <?php echo get_the_title(); ?>
+      <?php if($place) : ?>
+      &nbsp;@<?php echo $place; ?>
+      <?php endif; ?>
+    </div>
+  </div>
+
   <?php wp_reset_postdata(); ?>
+  <p>
+    <a href="<?php echo get_post_type_archive_link('schedule'); ?>" class="o-show__morelink">
+      More show
+    </a>
+  </p>
 </section>
 
 <?php
@@ -26,23 +44,31 @@ setup_postdata( $post );
 ?>
 
 <?php /* Video */ ?>
-<section>
-
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/<?php echo post_custom('video'); ?>?controls=0"
-    frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-    allowfullscreen></iframe>
-  <p>
-    <a href="<?php echo get_post_type_archive_link('videos'); ?>">More videos</a>
-  </p>
+<section class="o-featuredvideo">
+  <div class="o-featuredvideo__img">
+    <img src="http://img.youtube.com/vi/<?php echo $GLOBALS['featured_video']; ?>/maxresdefault.jpg" alt="">
+  </div>
+  <?php /* Video 
+  <iframe width="560" height="315"
+  src="https://www.youtube.com/embed/<?php echo $GLOBALS['featured_video']; ?>?controls=0" frameborder="0"
+  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  */ ?>
+  <a href="<?php echo get_post_type_archive_link('videos'); ?>" class="o-featuredvideo__link">More videos</a>
 </section>
 
 <?php /* SNS */ ?>
-<section>
-  <ul>
-    <li><?php echo post_custom('youtube'); ?></li>
-    <li><?php echo post_custom('twitter'); ?></li>
-    <li><?php echo post_custom('instagram'); ?></li>
-    <li><?php echo post_custom('facebook'); ?></li>
+<section class="o-sns">
+  <ul class="o-sns__list">
+    <?php foreach($GLOBALS['accounts'] as $account) : ?>
+    <li class="o-sns__item">
+      <a href="<?php echo $account['url']; ?>" target="_blank" class="o-sns__link">
+        <div class="o-sns__icon">
+          <img src="<?php echo get_template_directory_uri()."/assets/images/".$account['name'].".png"; ?>" alt="">
+        </div>
+        <div class="o-sns__id"><?php echo $account['id']; ?></div>
+      </a>
+    </li>
+    <?php endforeach; ?>
   </ul>
 </section>
 

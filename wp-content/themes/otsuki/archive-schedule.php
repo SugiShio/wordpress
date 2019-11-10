afb0e96e77a1b4ed8b0040a2da599535416ba603
Shoko Sugito <fruitmachine0@gmail.com>
2019-11-10 21:48:22 +0900

    タイトルのスタイル

--- wp-content/themes/otsuki/archive-schedule.php
+++ wp-content/themes/otsuki/archive-schedule.php
@@ -1,18 +1,27 @@
 <?php get_header(); ?>
-<?php
+<main class="o-main">
+  <div class="o-title">
+    <h2>
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/titles/schedule.svg" alt="schedule">
+    </h2>
+    <span class="o-title__sub">スケジュール</span>
+  </div>
+
+  <?php
   $posts = get_posts(array(
     'post_type' => 'schedule',
     'order' => 'ASC'
   ));
   ?>
-<ul>
-  <?php foreach($posts as $post) : ?>
-  <li>
-    <?php setup_postdata( $post ); ?>
-    <?php echo get_the_title(); ?>
-    <?php wp_reset_postdata(); ?>
-  </li>
-  <?php endforeach; ?>
-</ul>
+  <ul>
+    <?php foreach($posts as $post) : ?>
+    <li>
+      <?php setup_postdata( $post ); ?>
+      <?php echo get_the_title(); ?>
+      <?php wp_reset_postdata(); ?>
+    </li>
+    <?php endforeach; ?>
+  </ul>
+</main>
 
 <?php get_footer(); ?>
--- wp-content/themes/otsuki/page-contact.php
+++ wp-content/themes/otsuki/page-contact.php
@@ -1,3 +1,10 @@
 <?php get_header(); ?>
-contact
+<main class="o-main">
+  <div class="o-title">
+    <h2>
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/images/titles/contact.svg" alt="contact">
+    </h2>
+    <span class="o-title__sub">お問い合わせ</span>
+  </div>
+</main>
 <?php get_footer(); ?>

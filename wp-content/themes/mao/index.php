1aed01a24a2bd39819e4e22f06d056f4ce72fbb5
Shoko Sugito <fruitmachine0@gmail.com>
2020-05-02 16:11:58 +0900

    トップとそれ以外のページの遷移のアニメーション

--- wp-content/themes/mao/index.php
+++ wp-content/themes/mao/index.php
@@ -18,29 +18,31 @@
 ?>
 
 <div id="app" class="m-wrapper">
-  <div class="m-container" v-if='isTop'>
-    <?php include('pages/top.php'); ?>
-  </div>
-  <layout-default v-else :menu-items='<?php echo json_encode($menu_items); ?>'>
-    <template #about>
-      <?php include('pages/about.php'); ?>
-    </template>
+  <transition name='fade'>
+    <div class="m-container" v-if='isTop'>
+      <?php include('pages/top.php'); ?>
+    </div>
+    <layout-default v-else :menu-items='<?php echo json_encode($menu_items); ?>'>
+      <template #about>
+        <?php include('pages/about.php'); ?>
+      </template>
 
-    <template #works>
-      <?php include('pages/works.php'); ?>
-    </template>
+      <template #works>
+        <?php include('pages/works.php'); ?>
+      </template>
 
-    <template #schedule>
-      <?php include('pages/schedule.php'); ?>
-    </template>
+      <template #schedule>
+        <?php include('pages/schedule.php'); ?>
+      </template>
 
-    <template #goodfellows>
-      <?php include('pages/goodfellows.php'); ?>
-    </template>
+      <template #goodfellows>
+        <?php include('pages/goodfellows.php'); ?>
+      </template>
 
-    <template #contact>
-      <?php include('pages/contact.php'); ?>
-    </template>
-  </layout-default>
+      <template #contact>
+        <?php include('pages/contact.php'); ?>
+      </template>
+    </layout-default>
+  </transition>
 </div>
 <?php get_footer(); ?>

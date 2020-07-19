6d7e8771c2314eb5da464aad27fca493f80bfe9b
Shoko Sugito <fruitmachine0@gmail.com>
2020-07-19 22:51:26 +0900

    画像が変わったら画面サイズに合わせて画像サイズをセット

--- wp-content/themes/yuri-fukuoka/assets/js/imageViewer/viewWindow.vue
+++ wp-content/themes/yuri-fukuoka/assets/js/imageViewer/viewWindow.vue
@@ -1,20 +1,63 @@
 <template lang="pug">
 .background(@click='emitClose')
   .content
-    img.image(:src='src')
+    img.image(
+      :src='src'
+      :width='width'
+      :height='height'
+      )
     .title(v-if='title') {{ title }}
     .closeButton(@click='emitClose') close
 </template>
 
 <script>
+const MAX_IMAGE_RATIO = 0.8
 export default {
   props: {
     src: { type: String, default: '' },
     title: { type: String, default: '' }
   },
+  data() {
+    return {
+      width: null,
+      height: null
+    }
+  },
+  created() {
+    this.setWindowSize()
+  },
   methods: {
     emitClose() {
       this.$emit('close-button-clicked')
+    },
+    setWindowSize() {
+      this.windowWidth = window.innerWidth
+      this.windowHeight = window.innerHeight
+    }
+  },
+  watch: {
+    src(value) {
+      this.width = null
+      this.height = null
+      if (!value) return
+
+      this.$nextTick(() => {
+        this.setWindowSize()
+        const imageElement = this.$el.getElementsByClassName('image')[0]
+        const imageWidth = imageElement.width
+        const imageHeight = imageElement.height
+        if (
+          this.windowWidth * MAX_IMAGE_RATIO < imageWidth ||
+          this.windowHeight * MAX_IMAGE_RATIO < imageHeight
+        ) {
+          const ratio = Math.min(
+            (this.windowWidth * MAX_IMAGE_RATIO) / imageWidth,
+            (this.windowHeight * MAX_IMAGE_RATIO) / imageHeight
+          )
+          this.width = imageWidth * ratio
+          this.height = imageHeight * ratio
+        }
+      })
     }
   }
 }

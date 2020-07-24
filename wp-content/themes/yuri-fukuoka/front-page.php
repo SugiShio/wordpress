9f52d20788ced0f041f4e474377fe2b9e853fcf1
Shoko Sugito <fruitmachine0@gmail.com>
2020-07-24 17:13:04 +0900

    トップページ

---
+++ wp-content/themes/yuri-fukuoka/front-page.php
@@ -0,0 +1,52 @@
+<?php get_header(); ?>
+<div class="y-top">
+  <section class='y-mainview'>
+    <div class="y-mainview__flamenco">
+      <img src="<?php echo get_template_directory_uri()."/assets/images/main_flamenco.jpg"; ?>" alt="">
+
+      <div class="y-mainview__text">
+        <h3 class="y-mainview__subtext">下北沢のフラメンコ教室</h3>
+        <h2 class="y-mainview__maintext">Flamenco studio Las gracias</h2>
+      </div>
+    </div>
+    <div class="y-mainview__yoga">
+      <img src="<?php echo get_template_directory_uri()."/assets/images/main_yoga.jpg"; ?>" alt="">
+      <div class="y-mainview__text">
+        <h3 class="y-mainview__subtext">港区芝浦の少人数制ヨガサロン</h3>
+        <h2 class="y-mainview__maintext">yoga salon GRACIAGE</h2>
+      </div>
+    </div>
+  </section>
+
+  <section class="y-top__menu">
+    <?php include 'menu.php'; ?>
+  </section>
+
+  <section class="y-top__copy">
+    <img src="<?php echo get_template_directory_uri()."/assets/images/flowers_upper.png"; ?>" alt="">
+    <p>
+      当教室はフラメンコ・ヨガ・ピラティスのクラスを設けております。<br>
+      まずは体を動かして日々のストレスを発散し、<br>
+      体と心のスペースを取り戻し、<br>
+      さらに魅力的で健康的な自分と、<br>
+      充実した生活を見つけてみませんか？
+    </p>
+    <p class="y-top__maincopy">
+      <span>
+        <strong>カラダ</strong>を動かせば、<strong>ココロ</strong>が動く
+      </span>
+      <span><strong>ココロ</strong>が動けば、<strong>カラダ</strong>も動く</span>
+    </p>
+    <p>
+      日常の生活を変えたくても誰も変えてくれません。<br>
+      変えられるのはアナタです。<br>
+      まずは行動してみてください。<br>
+      きっと、今までと違う何かが見えてきます！<br>
+      心が動けばあなたの生活の質はもっともっと向上します。<br>
+      ​一緒に楽しい時間を共有しましょう！<br>
+    </p>
+    <img src="<?php echo get_template_directory_uri()."/assets/images/flowers_bottom.png"; ?>" alt="">
+  </section>
+</div>
+
+<?php get_footer(); ?>
